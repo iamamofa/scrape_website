561c0f38957ae8bd5f10d9b3ab7ea2eded2a69df
Justice Ohene Amofa <70078600+iamamofa@users.noreply.github.com>
2024-07-29 19:07:16 +0000

    environment activaiton and deactivate

---
+++ enviroments.sh
@@ -0,0 +1,18 @@
+# Create a virtual environment
+python3 -m venv myenv
+
+# Activate the virtual environment (macOS/Linux)
+source myenv/bin/activate
+
+# Or activate the virtual environment (Windows)
+myenv\Scripts\activate
+
+# Install Flask
+pip install flask
+
+# Install packages from requirements.txt
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
+
